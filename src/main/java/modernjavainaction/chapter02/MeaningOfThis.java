14ba8a5c2ad7b98b941cb71e28932346a6b9102b
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-11-16 19:55:31 +0300

    add: MeaningOfThis example from chapter 2

---
+++ src/main/java/modernjavainaction/chapter02/MeaningOfThis.java
@@ -0,0 +1,23 @@
+package modernjavainaction.chapter02;
+
+public class MeaningOfThis {
+    public final int value = 4;
+
+    public void doIt() {
+        int value = 6;
+        Runnable runnable = new Runnable() {
+            public final int value = 5;
+            @Override
+            public void run() {
+                int value = 10;
+                System.out.println(this.value);
+            }
+        };
+        runnable.run();
+    }
+
+    public static void main(String[] args) {
+        MeaningOfThis meaningOfThis = new MeaningOfThis();
+        meaningOfThis.doIt();
+    }
+}
