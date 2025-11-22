267d21238b69c35f082cb8ac1eda611a54c36f0c
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-11-22 20:12:22 +0300

    add: BuildingStreams example from chapter 5

--- src/main/java/modernjavainaction/chapter05/BuildingStreams.java
+++ src/main/java/modernjavainaction/chapter05/BuildingStreams.java
@@ -1,10 +1,11 @@
 package modernjavainaction.chapter05;
 
 import java.util.*;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class BuildingStreams {
-    public static void main(String[] args)  throws Exception {
+    public static void main(String[] args) throws Exception {
         Stream<String> stream = Stream.of("Java 8", "Lambdas", "In", "Action");
         stream
                 .map(String::toUpperCase)
@@ -15,5 +16,27 @@ public class BuildingStreams {
 
         int[] numbers = {2, 3, 5, 7, 11, 13};
         System.out.println(Arrays.stream(numbers).sum());
+
+        Stream.iterate(0, n -> n + 2)
+                .limit(10)
+                .forEach(System.out::println);
+
+        Stream.iterate(new int[]{0, 1},
+                        t -> new int[]{t[1], t[0] + t[1]})
+                .limit(20)
+                .forEach(t -> System.out.println("(" + t[0] + "," + t[1] + ")"));
+
+        Stream.iterate(new int[]{0, 1},
+                        t -> new int[]{t[1], t[0] + t[1]})
+                .limit(10)
+                .map(t -> t[0])
+                .forEach(System.out::println);
+
+        IntStream.iterate(0, n -> n < 100, n -> n + 4)
+                .forEach(System.out::println);
+
+        Stream.generate(Math::random)
+                .limit(5)
+                .forEach(System.out::println);
     }
 }
